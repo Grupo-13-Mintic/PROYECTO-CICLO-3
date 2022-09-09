eab493f01d363524fb646ed88d2f68a7729f3e4c
Kether <ketherleal@gmail.com>
2022-09-09 17:30:11 -0500

    borré carpeta repetida

--- ProyectoJava_Sprint_3/ProyectoJava/src/main/java/ProyectoJava/ProyectoJava/repository/IEmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package ProyectoJava.ProyectoJava.repository;
-
-import ProyectoJava.ProyectoJava.model.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmpleadoRepository extends JpaRepository<Empleado,Integer> {
-}
--- ProyectoJava_Sprint_3/ProyectoJava/src/main/java/ProyectoJava/ProyectoJava/repository/IEmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package ProyectoJava.ProyectoJava.repository;
-
-import ProyectoJava.ProyectoJava.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEmpresaRepository extends JpaRepository<Empresa,Integer> {
-}
